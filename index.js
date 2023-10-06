0f0037f04cc99e945f87844a69a5c1173481dab5
John <johnnkonge2020@gmail.com>
2023-10-06 14:38:44 +0300

    tried

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(word) {    
+  var l = word.length;
+  for (var i = 0; i < l / 2; i++) {
+      if (word.charAt(i) !== word.charAt(l - 1 - i)) {
+          return false;
+      }
+  }
+  return true;
+}
+
+if (isPalindrome("1122332211")) {
+  console.log("The word is a palindrome");
+} else {
+  console.log("The word is NOT a palindrome");
 }
 
 /* 
   Add your pseudocode here
+  declare a function that takes a string as its arguements,check if the input arg is Palindrome, console the result.
 */
 
 /*
@@ -11,6 +24,7 @@ function isPalindrome(word) {
 */
 
 // You can run `node index.js` to view these console logs
+/*
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
@@ -23,3 +37,14 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+function isPalindrome(str) {
+  var len = Math.floor(str.length / 2);
+  for (var i = 0; i < len; i++)
+    if (str[i] !== str[str.length - i - 1])
+      return false;
+  return true;
+}
+isPalindrome('racecar')
+*/
+
